e2ffbf8407310f8d2bb587fd18b86ee3b62aa9a5
koontz <koontz@cs.colostate.edu>
2015-10-26 21:03:59 -0600

    updating practice problems

--- algorithms/java/pow/pow.java
+++ algorithms/java/pow/pow.java
@@ -2,4 +2,34 @@ package pow;
 
 //todo
 public class pow {
+    public double powIter(double x, int n) {
+        if (n < 0) {
+            n = -n;
+            x = 1 / x;
+        }
+        double result = 1;
+        for (double f = x; n > 0; n = n >> 1) {
+            if (n % 2 == 1) {
+                result *= f;
+            }
+            f = f * f;
+        }
+        return result;
+    }
+
+    public double powRecurse(double x, int m) {
+        double temp = x;
+        if (m == 0)
+            return 1;
+        temp = powRecurse(x, m / 2);
+        if (m % 2 == 0)
+            return temp * temp;
+        else {
+            if (m > 0)
+                return x * temp * temp;
+            else
+                return (temp * temp) / x;
+        }
+
+    }
 }
---
+++ algorithms/java/pow/powTest.java
@@ -0,0 +1,18 @@
+package pow;
+
+import org.junit.Test;
+
+/**
+ * Created by jared on 10/26/15.
+ */
+public class powTest {
+
+    @Test
+    public void testMethod(){
+        pow p = new pow();
+        System.out.println(p.powIter(1, 2));
+        System.out.println(p.powRecurse(1, 2));
+
+    }
+
+}
