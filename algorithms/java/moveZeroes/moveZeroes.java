645aa7b37488742346af5dbbb11ffce2f82454b6
koontz <koontz@cs.colostate.edu>
2015-10-26 22:06:41 -0600

    Merge branch 'master' of github.com:haoel/leetcode
    Conflicts:
    	README.md
    	algorithms/cpp/moveZeroes/moveZeroes.cpp

--- algorithms/java/moveZeroes/moveZeroes.java
+++ algorithms/java/moveZeroes/moveZeroes.java
@@ -4,4 +4,20 @@ package moveZeroes;
  * Created by jared on 10/26/15.
  */
 public class moveZeroes {
+    // Shift non-zero values as far forward as possible
+// Fill remaining space with zeros
+
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+
+        int insertPos = 0;
+        for (int num: nums) {
+            if (num != 0) nums[insertPos++] = num;
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+
+    }
 }
