fbdac7f42b8c6c289c60272b8df7e4df59923f91
koontz <jkoontz09@gmail.com>
2015-12-21 13:02:31 -0700

    taking haoels recent changes
    (cherry picked from commit 5a39de8551d627263a80e2f63fdeaba0a935d575)
    
    # Conflicts:
    #	algorithms-java/twoSum/TwoSum.java
    #	algorithms-java/twoSumClass/TwoSumClass.java
    #	algorithms-java/twoSumSorted/TwoSumSorted.java
    #	algorithms/java/twoSum/TwoSum.java
    #	algorithms/java/twoSumClass/TwoSumClass.java
    #	algorithms/java/twoSumSorted/TwoSumSorted.java
    #	done/twoSum/TwoSum.java
    #	done/twoSumClass/TwoSumClass.java
    #	done/twoSumSorted/TwoSumSorted.java

--- algorithms/java/sumRootToLeafNumber/SumRootToLeafNumber.java
+++ algorithms/java/sumRootToLeafNumber/SumRootToLeafNumber.java
@@ -4,6 +4,7 @@ package sumRootToLeafNumber;
 // Date   : 2014-09-20 / 10-23-2015
 
 /**********************************************************************************
+ *
  * Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
  * An example is the root-to-leaf path 1->2->3 which represents the number 123.
  *
@@ -11,14 +12,16 @@ package sumRootToLeafNumber;
  *
  * For example,
  *
- * 1
- * / \
- * 2   3
+ *     1
+ *    / \
+ *   2   3
  *
  * The root-to-leaf path 1->2 represents the number 12.
  * The root-to-leaf path 1->3 represents the number 13.
  *
  * Return the sum = 12 + 13 = 25.
+ *
+ *
  **********************************************************************************/
 
 import common.TreeNode;
---
+++ todo/sudokuSolver/SudokuSolver.java
@@ -0,0 +1,25 @@
+package sudokuSolver;
+
+// Source : https://oj.leetcode.com/problems/sudoku-solver/
+// Author : Hao Chen & Jared Koontz
+// Date   : 2014-09-20 / 10-23-2015
+
+/**********************************************************************************
+ *
+ * Write a program to solve a Sudoku puzzle by filling the empty cells.
+ *
+ * Empty cells are indicated by the character '.'.
+ *
+ * You may assume that there will be only one unique solution.
+ *
+ * A sudoku puzzle...
+ *
+ * ...and its solution numbers marked in red.
+ *
+ *
+ **********************************************************************************/
+
+public class SudokuSolver {
+
+
+}
